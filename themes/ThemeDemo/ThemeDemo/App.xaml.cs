9ed72ca269c1e874cabf0c3f34e4b1065fb920f1
Frederic Vogels <frederic.vogels@ucll.be>
2021-03-26 17:41:12 +0100

    ThemeDemo

---
+++ themes/ThemeDemo/ThemeDemo/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ThemeDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public void SetTheme( string name )
+        {
+            //var sharedResourceDictionary = new ResourceDictionary() { Source = new Uri( $"Themes/Shared.xaml", UriKind.Relative ) };
+            //var resourceDictionary = new ResourceDictionary() { Source = new Uri( $"Themes/{name}.xaml", UriKind.Relative ) };
+
+            //Resources.Clear();
+            //Resources.MergedDictionaries.Clear();
+
+            ////Resources.MergedDictionaries.Add( sharedResourceDictionary );
+            //Resources.MergedDictionaries.Add( resourceDictionary );
+
+            var sharedResourceDictionary = new ResourceDictionary() { Source = new Uri( $"Themes/Shared.xaml", UriKind.Relative ) };
+            var resourceDictionary = new ResourceDictionary() { Source = new Uri( $"Themes/{name}.xaml", UriKind.Relative ) };
+            var resources = new ResourceDictionary();
+
+            resources.MergedDictionaries.Add( sharedResourceDictionary );
+            resources.MergedDictionaries.Add( resourceDictionary );
+
+            this.Resources = resources;
+        }
+    }
+}
---
+++ themes/ThemeDemo/ThemeDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThemeDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLightTheme( object sender, RoutedEventArgs e )
+        {
+            SetTheme( "Light" );
+        }
+
+        private void SetDarkTheme( object sender, RoutedEventArgs e )
+        {
+            SetTheme( "Dark" );
+        }
+
+        private void SetTheme(string name)
+        {
+            var app = (App) Application.Current;
+            app.SetTheme( name );
+        }
+    }
+}
